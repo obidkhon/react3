8f3c95fad54a289049eeddf6de454f3405ab4814
obidkon <obid.sharipov.80@mail.ru>
2020-10-31 18:31:50 +0500

    d

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import './App.css'
 import Counter from './Components/Counter'
+import Limmlim1 from './Components/Limmlim1'
+import Limmolim from './Components/Limmolim'
+import Limmolim2 from './Components/Limmolim2'
 
 import NEwmassage from './Components/NEwmassage'
 export class App extends Component {
@@ -9,6 +12,9 @@ export class App extends Component {
       <div className='App'>
         <NEwmassage/>
         <Counter/>
+        <Limmolim name='Akbarali' lastName= 'Olimov'/>
+        <Limmlim1 name='Akbarali' lastName= 'Olimov'/>
+        <Limmolim2  name='Akbarali' lastName= 'Olimov'/>
       </div>
     )
   }
